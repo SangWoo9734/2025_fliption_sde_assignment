604285bb5c9280759a6f22f7d029e77b9ff63523
Parksangwoo <psu8430@gmail.com>
2025-02-25 19:39:29 +0900

    style: 파일 경로, 색상 관련 tailwind 세팅 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      main: '#D95D28',
+      black: '#111111',
+      white: '#FFFFFF',
+      gray: {
+        900: '#1B1B1B',
+        800: '#2B2B2B',
+        700: '#616161',
+        600: '#757575',
+        500: '#9E9E9E',
+        400: '#B5B5B5',
+        300: '#E0E0E0',
+        200: '#EEEEEE',
+        100: '#F5F5F5',
+        50: '#FAFAFA',
       },
     },
   },
