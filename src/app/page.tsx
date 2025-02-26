744b4eeb084bb5b3e1890d18fbd5afd658c5c020
Parksangwoo <psu8430@gmail.com>
2025-02-26 23:11:45 +0900

    feat: 메인 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import { SERVICE_ROUTE } from '@/constants/routes';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='h-full flex justify-center items-center'>
+      <div className='flex flex-col items-center gap-20'>
+        <h1 className='text-4xl font-bold'>
+          Easiest Way to Swap Heads with Your Photos!
+        </h1>
+        <Link
+          href={SERVICE_ROUTE.newProject}
+          className='px-12 py-2 rounded-full text-2xl font-bold italic text-white bg-main'
+        >
+          Try it!
+        </Link>
+      </div>
+    </div>
+  );
 }
