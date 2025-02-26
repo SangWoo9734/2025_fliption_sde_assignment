0a8d27430c5ff2f938e20829511a1c53b76e027b
Parksangwoo <psu8430@gmail.com>
2025-02-26 23:19:37 +0900

    style: AppWrapper가 화면을 가득 채울 수 있도록 레이아웃 스타일 코드 수정

--- src/components/layout/AppWrapper.tsx
+++ src/components/layout/AppWrapper.tsx
@@ -4,7 +4,7 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='relative flex flex-col w-full max-w-[1200px] h-full m-auto '>
       <Navbar />
-      <main className='pt-24'>{children}</main>
+      <main className='flex-1 flex flex-col pt-24'>{children}</main>
     </div>
   );
 };
