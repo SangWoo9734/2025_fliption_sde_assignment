d9990bf2eda7b062acf530ae68c5f9f3e78e545c
Parksangwoo <psu8430@gmail.com>
2025-02-27 18:58:55 +0900

    chore: 불필요한 코드 삭제

--- src/apis/postFeedback.ts
+++ src/apis/postFeedback.ts
@@ -4,25 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 const postFeedback = async (feedbackForm: FeedbackFormType) => {
   const newFeedback: FeedbackType = {
     ...feedbackForm,
-    id: uuidv4(), // UUID 생성
+    id: uuidv4(),
     userId: 'user123',
     createdAt: new Date(),
     status: 'Answering',
   };
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/feedbacks`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newFeedback),
-    }
-  );
-
-  const data = await response.json();
-  console.log('등록된 피드백:', data);
+  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/feedbacks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newFeedback),
+  });
 };
 
 export default postFeedback;
