54e8c417c2d5d340182cb626e2faf75984cfd3c5
Parksangwoo <psu8430@gmail.com>
2025-02-27 23:25:02 +0900

    style: directory 내 컴포넌트 스타일 코드 수정

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -12,7 +12,10 @@ const DirectoryPage = () => {
   return (
     <ContentWrapper>
       <p className='mb-5 pb-5 border-b-2 text-xl'>
-        Total <span className='text-2xl font-bold'>{10}</span>
+        Total
+        <span className='pl-4 text-2xl font-bold text-main'>
+          {projectList.length}
+        </span>
       </p>
       <div className='grid grid-cols-4 gap-5'>
         {projectList.map((projectInfo, index) => (
--- src/components/page/DirectoryPage/ProjectCard.tsx
+++ src/components/page/DirectoryPage/ProjectCard.tsx
@@ -5,19 +5,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const ProjectCard = ({ projectInfo }: { projectInfo: ProjectType }) => {
-  const { id, title, resultImageUrl, status, updatedAt } = projectInfo;
+  const { id, title, sourceImageUrl, resultImageUrl, status, updatedAt } =
+    projectInfo;
 
   return (
     <Link
       href={`${SERVICE_ROUTE.directory}/${id}`}
       className='flex flex-col gap-2.5 w-full h-fit px-3 py-4 border border-gray-400 rounded-lg bg-main/10 hover:shadow-[0px_5px_10px_rgba(0,0,0,0.3)] transition-all ease-in-out duration-300'
     >
-      <div className='relative w-full aspect-[4/5] rounded-md overflow-hidden'>
+      <div className='relative w-full aspect-[4/5] border-2 border-gray-200 rounded-md overflow-hidden'>
         <Image
-          src={resultImageUrl}
+          src={status === 'done' ? resultImageUrl : sourceImageUrl}
           alt='project-thumbnail'
           fill={true}
-          className='object-cover'
+          className={`object-cover transition-all duration-300 ${
+            status !== 'done' ? 'blur-md' : ''
+          }`}
           priority
         />
       </div>
