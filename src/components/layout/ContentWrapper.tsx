0a3ae35e3a057df8656e36975bfd5e9e451246c2
Parksangwoo <psu8430@gmail.com>
2025-02-27 15:37:27 +0900

    style: ContentsWrapper 패딩 여백 사이즈 수정

--- src/components/layout/ContentWrapper.tsx
+++ src/components/layout/ContentWrapper.tsx
@@ -1,6 +1,6 @@
 const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex-1 flex flex-col px-9 py-6  rounded-t-xl shadow-[0px_5px_10px_rgba(0,0,0,0.3)]'>
+    <div className='flex-1 flex flex-col px-9 pt-6 pb-20 rounded-t-xl shadow-[0px_5px_10px_rgba(0,0,0,0.3)]'>
       {children}
     </div>
   );
