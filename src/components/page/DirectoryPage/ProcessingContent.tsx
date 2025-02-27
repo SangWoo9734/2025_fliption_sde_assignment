e6632c624d231d4706809354641abd6d81c82e5e
Parksangwoo <psu8430@gmail.com>
2025-02-27 15:26:10 +0900

    feat: 헤드 스왑 진행중 단계에서 프로젝트 상세 페이지에 보일 진행중 컨텐츠 추가

---
+++ src/components/page/DirectoryPage/ProcessingContent.tsx
@@ -0,0 +1,14 @@
+const ProcessingContent = () => {
+  return (
+    <>
+      <h1 className='text-3xl'>🎭</h1>
+      <p className='font-bold text-center'>
+        Applying the head swap magic…🪄
+        <br />
+        Almost there! Just a moment. ⏳
+      </p>
+    </>
+  );
+};
+
+export default ProcessingContent;
