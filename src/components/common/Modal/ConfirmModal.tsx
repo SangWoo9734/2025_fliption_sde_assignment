b32379cbca9e1fab0aaf76f9bb6e57178da8d869
Parksangwoo <psu8430@gmail.com>
2025-02-27 18:22:19 +0900

    chore: 확인 모달 컴포넌트 내 버튼 onClick Event 부여

--- src/components/common/Modal/ConfirmModal.tsx
+++ src/components/common/Modal/ConfirmModal.tsx
@@ -1,6 +1,18 @@
+import { SERVICE_ROUTE } from '@/constants/routes';
+import modalStore from '@/store/modalStore';
+import { useRouter } from 'next/navigation';
+import { useStore } from 'zustand';
 import Button from '../Button';
 
 const ConfirmModal = () => {
+  const router = useRouter();
+  const { closeModal } = useStore(modalStore);
+
+  const clickMoveFeedbackButton = () => {
+    closeModal();
+    router.push(SERVICE_ROUTE.feedback);
+  };
+
   return (
     <div>
       <p className='text-center py-20 font-bold'>
@@ -9,10 +21,12 @@ const ConfirmModal = () => {
         We’ll adjust the swap if necessary.
       </p>
       <div className='flex gap-2.5'>
-        <Button isFull={true} theme='default'>
+        <Button isFull={true} theme='default' onClick={closeModal}>
           OK
         </Button>
-        <Button isFull={true}>Move Feedback</Button>
+        <Button isFull={true} onClick={clickMoveFeedbackButton}>
+          Move Feedback
+        </Button>
       </div>
     </div>
   );
