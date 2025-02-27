3557cd5728e7da7f628a2f7948b3252dad559f0b
Parksangwoo <psu8430@gmail.com>
2025-02-27 15:26:31 +0900

    feat: 헤드 스왑 대기중 단계에서 프로젝트 상세 페이지에 보일 대기중 컨텐츠 추가

---
+++ src/components/page/DirectoryPage/WaitingContent.tsx
@@ -0,0 +1,14 @@
+const WaitingContent = () => {
+  return (
+    <>
+      <h1 className='text-3xl'>🚀</h1>
+      <p className='font-bold text-center'>
+        We are reviewing your request! ✨
+        <br />
+        Hang tight—your awesome result is on the way!
+      </p>
+    </>
+  );
+};
+
+export default WaitingContent;
